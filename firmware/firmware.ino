1823ff0494b9e58915868faf673c06e0b0e04bd6
adamblumoff <ablumoff25@amherst.edu>
2026-03-17 17:38:28 -0500

    fix: bump firmware version to 0.5.4

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -10,7 +10,7 @@
 #include <esp_system.h>
 #include <mbedtls/sha256.h>
 
-const char* FIRMWARE_VERSION = "0.5.3";
+const char* FIRMWARE_VERSION = "0.5.4";
 const int PROVISION_RESET_PIN = 0;
 const char* PREFS_NAMESPACE = "gym-motion";
 const char* PREF_DEVICE_ID = "device_id";
